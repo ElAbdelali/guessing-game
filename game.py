c8d0cc0348897af5b2f64bfbe887df5be89ac51d
ElAbdelali <eljaouhariabdelali@outlook.com>
2023-04-06 00:47:26 -0500

    Will complete code tomorrow

--- game.py
+++ game.py
@@ -1,3 +1,13 @@
+import random
 """A number-guessing game."""
 
-# Put your code here
+t_or_f = True
+guess = 0
+name = input("Howdy, enter your name please: ")
+print(f"Hello {name}, welcome to the guessing game!")
+
+random_number = random(1,100)
+
+while t_or_f:
+    
+    guess = input("Please input a number between 1 and 100: ")
